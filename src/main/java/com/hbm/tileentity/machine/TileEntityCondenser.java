9bfa740406f3623e75a1045728f0ad1ccac046aa
Your Name <you@example.com>
2026-03-17 08:27:32 +0500

    mmm condensers

--- src/main/java/com/hbm/tileentity/machine/TileEntityCondenser.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityCondenser.java
@@ -30,9 +30,11 @@ public class TileEntityCondenser extends TileEntityLoadedBase implements IFluidS
 	public int waterTimer = 0;
 	protected int throughput;
 	public boolean hasAtmosphericPressure = true;
+	private boolean atmospherePressureInitialized = false;
 
 	public boolean vacuumOptimised = false;
 	private static final double MIN_CONDENSATION_PRESSURE = 0.01D;
+	private static final int ATMOSPHERE_CHECK_INTERVAL_TICKS = 10;
 
 	//Configurable values
 	public static int inputTankSize = 100;
@@ -75,7 +77,15 @@ public class TileEntityCondenser extends TileEntityLoadedBase implements IFluidS
 				this.waterTimer--;
 
 			int convert = Math.min(tanks[0].getFill(), tanks[1].getMaxFill() - tanks[1].getFill());
-			this.hasAtmosphericPressure = hasRequiredAtmosphericPressure();
+
+			if(vacuumOptimised) {
+				this.hasAtmosphericPressure = true;
+				this.atmospherePressureInitialized = true;
+			} else if(shouldRefreshAtmospherePressure()) {
+				this.hasAtmosphericPressure = hasRequiredAtmosphericPressure();
+				this.atmospherePressureInitialized = true;
+			}
+
 			boolean canCondense = vacuumOptimised || this.hasAtmosphericPressure;
 			this.throughput = canCondense ? convert : 0;
 
@@ -108,6 +118,15 @@ public class TileEntityCondenser extends TileEntityLoadedBase implements IFluidS
 	public void packExtra(NBTTagCompound data) { }
 	public boolean extraCondition(int convert) { return true; }
 	public void postConvert(int convert) { }
+
+	private boolean shouldRefreshAtmospherePressure() {
+		if(!this.atmospherePressureInitialized) return true;
+
+		int offset = (xCoord * 31 + yCoord * 17 + zCoord * 13) % ATMOSPHERE_CHECK_INTERVAL_TICKS;
+		if(offset < 0) offset += ATMOSPHERE_CHECK_INTERVAL_TICKS;
+
+		return worldObj.getTotalWorldTime() % ATMOSPHERE_CHECK_INTERVAL_TICKS == offset;
+	}
 	
 	private boolean hasRequiredAtmosphericPressure() {
 		CBT_Atmosphere atmosphere = ChunkAtmosphereManager.proxy.getAtmosphere(worldObj, xCoord, yCoord, zCoord);
